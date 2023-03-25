afb5124e14097bc713e0db5ce3b6718297f14f1f
devcosmos <va-litv@yandex.ru>
2023-03-25 20:11:54 +0300

    create useMap hook

---
+++ project/src/hooks/useMap.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useRef, useState, MutableRefObject } from 'react';
+import leaflet from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import { City } from '../types/city';
+
+function useMap(mapRef: MutableRefObject<HTMLElement | null>, city: City) {
+  const [map, setMap] = useState<leaflet.Map | null>(null);
+  const isRenderedRef = useRef<boolean>(false);
+
+  useEffect(() => {
+    if (mapRef.current !== null && !isRenderedRef.current) {
+      const instance = leaflet.map(mapRef.current, {
+        center: {
+          lat: city.lat,
+          lng: city.lng,
+        },
+        zoom: 10,
+      });
+
+      leaflet
+        .tileLayer(
+          'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png'
+        )
+        .addTo(instance);
+
+      setMap(instance);
+      isRenderedRef.current = true;
+    }
+  }, [mapRef, city]);
+
+  return map;
+}
+
+export default useMap;
