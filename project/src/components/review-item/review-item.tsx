b1b7aee65df38373915906136e01fa5edec2b19c
devcosmos <va-litv@yandex.ru>
2023-03-28 21:56:03 +0300

    create review-item component

---
+++ project/src/components/review-item/review-item.tsx
@@ -0,0 +1,37 @@
+import { Review } from '../../types/review';
+
+type ReviewItemProps = {
+  review: Review;
+}
+
+function ReviewItem({review}:ReviewItemProps): JSX.Element {
+  const {user, rating, date, textReview} = review;
+  const dateObj = new Date(date);
+
+  return (
+    <li className="reviews__item">
+      <div className="reviews__user user">
+        <div className="reviews__avatar-wrapper user__avatar-wrapper">
+          <img className="reviews__avatar user__avatar" src={user.avatar} width="54" height="54" alt={user.name} />
+        </div>
+        <span className="reviews__user-name">
+          {user.name}
+        </span>
+      </div>
+      <div className="reviews__info">
+        <div className="reviews__rating rating">
+          <div className="reviews__stars rating__stars">
+            <span style={{ width: `${Math.round(rating) * 20}%` }}></span>
+            <span className="visually-hidden">Rating</span>
+          </div>
+        </div>
+        <p className="reviews__text">{textReview}</p>
+        <time className="reviews__time" dateTime={dateObj.toISOString().split('T')[0]}>
+          {dateObj.toLocaleString('en', { month: 'long', year: 'numeric' })}
+        </time>
+      </div>
+    </li>
+  );
+}
+
+export default ReviewItem;
