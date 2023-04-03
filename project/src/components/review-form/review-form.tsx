61bfb765539e42f5dfb0486a60bba61317075459
devcosmos <va-litv@yandex.ru>
2023-04-03 21:26:18 +0300

    rename handler

--- project/src/components/review-form/review-form.tsx
+++ project/src/components/review-form/review-form.tsx
@@ -8,23 +8,23 @@ function ReviewForm(): JSX.Element {
     review: '',
   });
 
-  const fieldChangeHandle = (evt: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const fieldChangeHandler = (evt: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const {name, value} = evt.target;
     setFormData({...formData, [name]: value});
   };
 
-  const formSubmitHandle = (evt: FormEvent<HTMLFormElement>) => {
+  const formSubmitHandler = (evt: FormEvent<HTMLFormElement>) => {
     evt.preventDefault();
   };
 
   return (
-    <form className="reviews__form form" action="#" method="post" onSubmit={formSubmitHandle}>
+    <form className="reviews__form form" action="#" method="post" onSubmit={formSubmitHandler}>
       <label className="reviews__label form__label" htmlFor="review">Your review</label>
       <div className="reviews__rating-form form__rating">
-        {RATING_LABELS.map((title, i, arr) => <RatingInput key={title} count={arr.length - i} title={title} fieldChangeHandle={fieldChangeHandle} />)}
+        {RATING_LABELS.map((title, i, arr) => <RatingInput key={title} count={arr.length - i} title={title} fieldChangeHandler={fieldChangeHandler} />)}
       </div>
       <textarea
-        onChange={fieldChangeHandle}
+        onChange={fieldChangeHandler}
         className="reviews__textarea form__textarea"
         id="review"
         value={formData.review}
