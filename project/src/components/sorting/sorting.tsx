c018829a6e0467f22e3856782963a0962bed8058
devcosmos <va-litv@yandex.ru>
2023-04-05 11:57:51 +0300

    create new component - sorting

---
+++ project/src/components/sorting/sorting.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import { useAppDispatch } from '../../hooks';
+import { SORTING_OPTIONS } from '../../const';
+import { changeSorting } from '../../store/action';
+
+type SortingProps = {
+  selectedSorting: string;
+}
+
+function Sorting({selectedSorting}: SortingProps): JSX.Element {
+  const [isOpenSorting, setOpenSorting] = useState<boolean>(false);
+
+  function openSortingHandler() {
+    setOpenSorting(!isOpenSorting);
+  }
+
+  const dispatch = useAppDispatch();
+
+  function chooseSortHandler(sorting: string) {
+    dispatch(changeSorting(sorting));
+    setOpenSorting(false);
+  }
+
+  return (
+    <form className="places__sorting" action="#" method="get">
+      <span className="places__sorting-caption">Sort by </span>
+      <span className="places__sorting-type" tabIndex={0} onClick={openSortingHandler}>
+        {selectedSorting}
+        <svg className="places__sorting-arrow" width="7" height="4">
+          <use xlinkHref="#icon-arrow-select"></use>
+        </svg>
+      </span>
+      {isOpenSorting &&
+        <ul className="places__options places__options--custom places__options--opened">
+          {SORTING_OPTIONS.map((item) => {
+            const activeClass = item === selectedSorting ? ' places__option--active' : '';
+            const classes = `places__option${activeClass}`;
+
+            return <li key={item} className={classes} tabIndex={0} onClick={() => chooseSortHandler(item)}>{item}</li>;
+          })}
+        </ul>}
+    </form>
+  );
+}
+
+export default Sorting;
