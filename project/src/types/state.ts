a1f43bce4775a08dd4858789b84a079341c21cdf
devcosmos <va-litv@yandex.ru>
2023-04-17 21:17:26 +0300

    create user-process

--- project/src/types/state.ts
+++ project/src/types/state.ts
@@ -1,4 +1,25 @@
 import { store } from '../store/index';
+import { City, Offer } from '../types/offer';
+import { Review } from '../types/review';
+import { UserData } from '../types/user-data';
+import { AuthorizationStatus, SortingOption } from '../const';
+
+export type UserProcess = {
+  authorizationStatus: AuthorizationStatus;
+  user: UserData | null;
+};
+
+export type DataProcess = {
+  offers: Offer[];
+  nearPlacesOffers: Offer[];
+  reviews: Review[];
+  isOffersDataLoading: boolean;
+};
+
+export type OffersProcess = {
+  city: City;
+  sorting: SortingOption;
+};
 
 export type State = ReturnType<typeof store.getState>;
 
