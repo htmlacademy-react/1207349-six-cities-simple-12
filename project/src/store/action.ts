cf309aac0656ce07c8e115eeb2b95ccefa1285b3
devcosmos <va-litv@yandex.ru>
2023-04-17 21:22:24 +0300

    remove useless actions

--- project/src/store/action.ts
+++ project/src/store/action.ts
@@ -1,26 +1,4 @@
 import { createAction } from '@reduxjs/toolkit';
-import { City } from '../types/offer';
-import { Offer } from '../types/offer';
-import { Review } from '../types/review';
-import { UserData } from '../types/user-data';
-import { AppRoute, AuthorizationStatus, SortingOption } from '../const';
-
-export const changeCity = createAction<City>('offers/changeCity');
-
-export const changeSorting = createAction<SortingOption>('offers/changeSorting');
-
-export const loadOffers = createAction<Offer[]>('data/loadOffers');
-
-export const loadNearPlacesOffers = createAction<Offer[]>('data/loadNearPlacesOffers');
-
-export const loadReviews = createAction<Review[]>('data/loadReviews');
-
-export const publishReview = createAction<Review[]>('data/publishReviews');
-
-export const loadUser = createAction<UserData | null>('data/loadUser');
-
-export const setOffersDataLoadingStatus = createAction<boolean>('data/setOffersDataLoadingStatus');
-
-export const requireAuthorization = createAction<AuthorizationStatus>('user/requireAuthorization');
+import { AppRoute } from '../const';
 
 export const redirectToRoute = createAction<AppRoute>('app/redirectToRoute');
