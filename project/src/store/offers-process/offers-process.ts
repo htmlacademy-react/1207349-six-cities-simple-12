b6b863e156bc8eba860f965c61ec128037ce36be
devcosmos <va-litv@yandex.ru>
2023-04-19 15:33:14 +0300

    style import add spaces

--- project/src/store/offers-process/offers-process.ts
+++ project/src/store/offers-process/offers-process.ts
@@ -1,4 +1,4 @@
-import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { OffersProcess } from '../../types/state';
 import { city, NameSpace, SortingOption } from '../../const';
 import { City } from '../../types/offer';
