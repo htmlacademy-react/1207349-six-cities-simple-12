854ee4a34c583fc24887e2576d09d85b955a5075
devcosmos <va-litv@yandex.ru>
2023-04-19 18:30:39 +0300

    create offers data test

--- project/src/utils/mocks.ts
+++ project/src/utils/mocks.ts
@@ -1,5 +1,8 @@
-import { datatype, internet } from 'faker';
+import { address, company, datatype, date, image, internet, random } from 'faker';
 import { UserData } from '../types/user-data';
+import { Offer } from '../types/offer';
+import { lorem } from 'faker';
+import { Review } from '../types/review';
 
 export const makeFakeUser = (): UserData => ({
   avatarUrl: internet.avatar(),
@@ -9,3 +12,54 @@ export const makeFakeUser = (): UserData => ({
   name: internet.userName(),
   token: datatype.uuid(),
 } as UserData);
+
+const makeFakeOffer = (): Offer => ({
+  bedrooms: datatype.number(5),
+  city: {
+    name: address.cityName(),
+    location: {
+      latitude: Number(address.latitude()),
+      longitude: Number(address.latitude()),
+    }
+  },
+  description: lorem.paragraph(),
+  goods: new Array(datatype.number(5)).fill(null).map(() => random.words()),
+  host: {
+    id: datatype.number(10),
+    avatarUrl: internet.avatar(),
+    name: internet.userName(),
+    isPro: datatype.boolean(),
+  },
+  id: datatype.number(10),
+  images: new Array(datatype.number(5)).fill(null).map(() => image.city()),
+  isPremium: datatype.boolean(),
+  location: {
+    latitude: Number(address.latitude()),
+    longitude: Number(address.latitude()),
+  },
+  maxAdults: datatype.number(10),
+  previewImage: image.city(),
+  price: datatype.number(),
+  rating: datatype.number(5),
+  title: company.companyName(),
+  type: random.word(),
+} as Offer);
+
+export const makeFakeOffers = (quantity?: number): Offer[] =>
+  new Array(quantity ?? datatype.number(15)).fill(null).map(() => makeFakeOffer());
+
+const makeFakeReview = (): Review => ({
+  id: datatype.number(10),
+  user: {
+    avatarUrl: internet.avatar(),
+    id: datatype.number(10),
+    isPro: datatype.boolean(),
+    name: internet.userName(),
+  },
+  rating: datatype.number(5),
+  date: date.past().toDateString(),
+  comment: lorem.paragraph(3),
+} as unknown as Review);
+
+export const makeFakeReviews = (quantity?: number): Review[] =>
+  new Array(quantity ?? datatype.number(10)).fill(null).map(() => makeFakeReview());
