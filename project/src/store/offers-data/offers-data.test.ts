2ca76bf62cde2841937886cb4d754f2bc4f9682c
devcosmos <va-litv@yandex.ru>
2023-04-24 11:53:32 +0300

    refactor toEqual

--- project/src/store/offers-data/offers-data.test.ts
+++ project/src/store/offers-data/offers-data.test.ts
@@ -4,123 +4,79 @@ import { RequestStatus } from '../../const';
 import { fetchNearPlacesOffersAction, fetchOffersAction, fetchReviewsAction, publishReviewAction } from '../api-actions';
 import { makeFakeOffers, makeFakeReviews } from '../../utils/mocks';
 
-const offers = makeFakeOffers();
-const nearPlacesOffers = makeFakeOffers(3);
-const reviews = makeFakeReviews();
+const fakeOffers = makeFakeOffers();
+const fakeNearPlacesOffers = makeFakeOffers(3);
+const fakeReviews = makeFakeReviews();
+
+function getFakeState({
+  offers = [],
+  nearPlacesOffers = [],
+  reviews = [],
+  isOffersDataLoading = false,
+  reviewsPublishStatus = RequestStatus.Idle,
+}: Partial<DataProcess>): DataProcess {
+  return ({
+    offers,
+    nearPlacesOffers,
+    reviews,
+    isOffersDataLoading,
+    reviewsPublishStatus,
+  });
+}
 
 describe('Reducer: offersData', () => {
   let state: DataProcess;
 
   beforeEach(() => {
-    state = {
-      offers: [],
-      nearPlacesOffers: [],
-      reviews: [],
-      isOffersDataLoading: false,
-      reviewsPublishStatus: RequestStatus.Idle,
-    };
+    state = getFakeState({});
   });
 
   it('without additional parameters should return initial state', () => {
     expect(offersData.reducer(undefined, {type: 'UNKNOWN_ACTION'}))
-      .toEqual({
-        offers: [],
-        nearPlacesOffers: [],
-        reviews: [],
-        isOffersDataLoading: false,
-        reviewsPublishStatus: RequestStatus.Idle,
-      });
+      .toEqual(getFakeState({}));
   });
 
   describe('fetchOffersAction test', () => {
     it('should update isOffersDataLoading to true if fetchOffersAction pending', () => {
       expect(offersData.reducer(state, { type: fetchOffersAction.pending.type }))
-        .toEqual({
-          offers: [],
-          nearPlacesOffers: [],
-          reviews: [],
-          isOffersDataLoading: true,
-          reviewsPublishStatus: RequestStatus.Idle,
-        });
+        .toEqual(getFakeState({isOffersDataLoading: true}));
     });
     it('should update isOffersDataLoading to false and offers to load offers if fetchOffersAction fulfilled', () => {
-      expect(offersData.reducer(state, { type: fetchOffersAction.fulfilled.type, payload: offers }))
-        .toEqual({
-          offers,
-          nearPlacesOffers: [],
-          reviews: [],
-          isOffersDataLoading: false,
-          reviewsPublishStatus: RequestStatus.Idle,
-        });
+      expect(offersData.reducer(state, { type: fetchOffersAction.fulfilled.type, payload: fakeOffers }))
+        .toEqual(getFakeState({offers: fakeOffers}));
     });
     it('should return initial state if fetchOffersAction rejected', () => {
       expect(offersData.reducer(state, { type: fetchOffersAction.rejected.type }))
-        .toEqual({
-          offers: [],
-          nearPlacesOffers: [],
-          reviews: [],
-          isOffersDataLoading: false,
-          reviewsPublishStatus: RequestStatus.Idle,
-        });
+        .toEqual(getFakeState({}));
     });
   });
 
   describe('fetchNearPlacesOffersAction test', () => {
     it('should update nearPlacesOffers to load nearPlacesOffers', () => {
-      expect(offersData.reducer(state, { type: fetchNearPlacesOffersAction.fulfilled.type, payload: nearPlacesOffers }))
-        .toEqual({
-          offers: [],
-          nearPlacesOffers,
-          reviews: [],
-          isOffersDataLoading: false,
-          reviewsPublishStatus: RequestStatus.Idle,
-        });
+      expect(offersData.reducer(state, { type: fetchNearPlacesOffersAction.fulfilled.type, payload: fakeNearPlacesOffers }))
+        .toEqual(getFakeState({nearPlacesOffers: fakeNearPlacesOffers}));
     });
   });
 
   describe('fetchReviewsAction test', () => {
     it('should update reviews to load reviews', () => {
-      expect(offersData.reducer(state, { type: fetchReviewsAction.fulfilled.type, payload: reviews }))
-        .toEqual({
-          offers: [],
-          nearPlacesOffers: [],
-          reviews,
-          isOffersDataLoading: false,
-          reviewsPublishStatus: RequestStatus.Idle,
-        });
+      expect(offersData.reducer(state, { type: fetchReviewsAction.fulfilled.type, payload: fakeReviews }))
+        .toEqual(getFakeState({reviews: fakeReviews}));
     });
   });
 
   describe('publishReviewAction test', () => {
     it('should update reviewsPublishStatus to "PENDING" if publishReviewAction pending', () => {
       expect(offersData.reducer(state, { type: publishReviewAction.pending.type }))
-        .toEqual({
-          offers: [],
-          nearPlacesOffers: [],
-          reviews: [],
-          isOffersDataLoading: false,
-          reviewsPublishStatus: RequestStatus.Pending,
-        });
+        .toEqual(getFakeState({reviewsPublishStatus: RequestStatus.Pending}));
     });
     it('should update reviewsPublishStatus to "FULFILLED" and reviews to load reviews if publishReviewAction fulfilled', () => {
-      expect(offersData.reducer(state, { type: publishReviewAction.fulfilled.type, payload: reviews }))
-        .toEqual({
-          offers: [],
-          nearPlacesOffers: [],
-          reviews,
-          isOffersDataLoading: false,
-          reviewsPublishStatus: RequestStatus.Fulfilled,
-        });
+      expect(offersData.reducer(state, { type: publishReviewAction.fulfilled.type, payload: fakeReviews }))
+        .toEqual(getFakeState({reviews: fakeReviews, reviewsPublishStatus: RequestStatus.Fulfilled}));
     });
     it('should update reviewsPublishStatus to "REJECTED" if publishReviewAction rejected', () => {
       expect(offersData.reducer(state, { type: publishReviewAction.rejected.type }))
-        .toEqual({
-          offers: [],
-          nearPlacesOffers: [],
-          reviews: [],
-          isOffersDataLoading: false,
-          reviewsPublishStatus: RequestStatus.Rejected,
-        });
+        .toEqual(getFakeState({reviewsPublishStatus: RequestStatus.Rejected}));
     });
   });
 });
